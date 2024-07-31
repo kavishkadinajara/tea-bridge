0a61634613d3a53510638f3316d166674e733436
lafesto <kavishkadinajara@gmail.com>
2024-07-31 20:13:04 +0530

    "Refactored LoginPage component: reformatted code, removed unused imports, and updated JSX syntax"

--- app/(authkit)/login/page.tsx
+++ app/(authkit)/login/page.tsx
@@ -1,22 +1,22 @@
+/* eslint-disable react/no-unescaped-entities */
 "use client";
 
 // Import necessary modules and components
-import { FaGithub, FaGoogle, FaEye, FaEyeSlash, FaEnvelope, FaLock } from 'react-icons/fa';
-import { motion } from 'framer-motion';
+import { FaEye, FaEyeSlash, FaEnvelope, FaLock } from "react-icons/fa";
+import { motion } from "framer-motion";
 import Link from "next/link";
-import LoginHero from "@/components/LoginHero";
-import GoogleButton from "@/components/GoogleButton";
 import Image from "next/image";
 import { Input } from "@nextui-org/input";
 import React, { useState } from "react";
-import clsx from 'clsx';
-import {Select, SelectItem} from "@nextui-org/react";
-import {SelectorIcon} from "@/components/SelectorIcon";
-import {userType} from "@/config/data";
+import clsx from "clsx";
+import { Select, SelectItem } from "@nextui-org/react";
+
+import LoginHero from "@/components/LoginHero";
+import { SelectorIcon } from "@/components/SelectorIcon";
+import { userType } from "@/config/data";
 
 // Define the LoginPage component
-export default function LoginPage({ status }: { status: string }) {
-
+export default function LoginPage({}: { status: string }) {
   // State to manage password visibility
   const [isVisible, setIsVisible] = React.useState(false);
   const toggleVisibilityR = () => setIsVisible(!isVisible);
@@ -27,12 +27,13 @@ export default function LoginPage({ status }: { status: string }) {
 
   // Function to toggle between "Sign In" and "Sign Up"
   function toggleTwoAuth() {
-    setAuthCard(prevAuthCard => prevAuthCard === "Sign In" ? "Sign Up" : "Sign In");
+    setAuthCard((prevAuthCard) =>
+      prevAuthCard === "Sign In" ? "Sign Up" : "Sign In",
+    );
   }
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 w-full justify-center items-center space-x-12">
-      
       {/* Left section with a hero image, visible only on large screens */}
       <div className="hidden lg:block lg:col-span-1">
         <LoginHero />
@@ -41,57 +42,73 @@ export default function LoginPage({ status }: { status: string }) {
       {/* Right section with the login form */}
       <div className="flex flex-col justify-center items-center min-h-[680px] mt-4">
         <div className="lg:col-span-1 md:shadow-lg p-6 w-full md:shadow-cyan-600 md:hover:shadow-lime-500 rounded-3xl">
-          
           {/* Logo and heading */}
           <div className="flex justify-center md:mb-4">
-            <Link href={'/'}><Image src={"/logo.png"} alt={"logo"} width={90} height={90} className="w-12 h-12 lg:w-24 lg:h-24" /></Link>
-            <h1 className="text-lg md:text-2xl lg:text-3xl text-center font-bold mt-3 md:mt-8">{authCard}</h1><br />
+            <Link href={"/"}>
+              <Image
+                alt={"logo"}
+                className="w-12 h-12 lg:w-24 lg:h-24"
+                height={90}
+                src={"/logo.png"}
+                width={90}
+              />
+            </Link>
+            <h1 className="text-lg md:text-2xl lg:text-3xl text-center font-bold mt-3 md:mt-8">
+              {authCard}
+            </h1>
+            <br />
           </div>
 
           {/* Container for both login methods */}
           <div className="flex justify-center items-center">
-            
             {/* First card for register */}
             <div className="flex justify-center items-center">
               <motion.div
-                animate={{ opacity: authCard === "Sign In" ? 0.5 : 1, scale: authCard === "Sign In" ? 0.75 : 1 }}
-                transition={{ duration: 0.5 }}
+                animate={{
+                  opacity: authCard === "Sign In" ? 0.5 : 1,
+                  scale: authCard === "Sign In" ? 0.75 : 1,
+                }}
                 className={clsx(
                   "p-8 rounded-2xl shadow-lg shadow-cyan-600 hover:shadow-lime-500 w-full max-w-sm relative mb-6",
                   {
-                    "left-16 backdrop-blur-3xl blur-[2px] z-0": authCard === "Sign In",
-                    "left-28 md:left-32 bg-green-100 dark:bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-[#0a0015] via-[#00150e] to-black z-20": authCard === "Sign Up"
-                  }
+                    "left-16 backdrop-blur-3xl blur-[2px] z-0":
+                      authCard === "Sign In",
+                    "left-28 md:left-32 bg-green-100 dark:bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-[#0a0015] via-[#00150e] to-black z-20":
+                      authCard === "Sign Up",
+                  },
                 )}
+                transition={{ duration: 0.5 }}
               >
                 <form>
-                <Select
-                  label="User Type"
-                  placeholder="Select who are you?"
-                  labelPlacement="outside"
-                  className="max-w-xs"
-                  disableSelectorIconRotation
-                  selectorIcon={<SelectorIcon />}
-                  disabled={authCard === "Sign In"}
-                >
-                  {userType.map((userType) => (
-                    <SelectItem key={userType.key}
-                    isDisabled={authCard === "Sign In"}>
-                      {userType.label}
-                    </SelectItem>
-                  ))}
-                </Select>
-                <br />
-                
+                  <Select
+                    disableSelectorIconRotation
+                    className="max-w-xs"
+                    disabled={authCard === "Sign In"}
+                    label="User Type"
+                    labelPlacement="outside"
+                    placeholder="Select who are you?"
+                    selectorIcon={<SelectorIcon />}
+                  >
+                    {userType.map((userType) => (
+                      <SelectItem
+                        key={userType.key}
+                        isDisabled={authCard === "Sign In"}
+                      >
+                        {userType.label}
+                      </SelectItem>
+                    ))}
+                  </Select>
+                  <br />
+
                   {/* Email input */}
                   <div className="mt-5">
                     <Input
-                      type="email"
-                      label="Email"
-                      placeholder="you@example.com"
-                      labelPlacement="outside"
-                      startContent={<FaEnvelope className="text-default-400" />}
                       disabled={authCard === "Sign In"}
+                      label="Email"
+                      labelPlacement="outside"
+                      placeholder="you@example.com"
+                      startContent={<FaEnvelope className="text-default-400" />}
+                      type="email"
                     />
                   </div>
                   <br />
@@ -99,17 +116,14 @@ export default function LoginPage({ status }: { status: string }) {
                   {/* Password input */}
                   <div className="mt-5">
                     <Input
-                      type={isVisible ? "text" : "password"}
-                      label="Password"
-                      placeholder="Enter your password"
-                      labelPlacement="outside"
-                      startContent={<FaLock className="text-default-400" />}
+                      className="md:w-[300px] w-[225px]"
+                      disabled={authCard === "Sign In"}
                       endContent={
                         <button
+                          aria-label="toggle password visibility"
                           className="focus:outline-none"
                           type="button"
                           onClick={toggleVisibilityR}
-                          aria-label="toggle password visibility"
                           // disabled={authCard === "Sign In"}
                         >
                           {isVisible ? (
@@ -119,8 +133,11 @@ export default function LoginPage({ status }: { status: string }) {
                           )}
                         </button>
                       }
-                      className="md:w-[300px] w-[225px]"
-                      disabled={authCard === "Sign In"}
+                      label="Password"
+                      labelPlacement="outside"
+                      placeholder="Enter your password"
+                      startContent={<FaLock className="text-default-400" />}
+                      type={isVisible ? "text" : "password"}
                     />
                   </div>
                   {/* <div className="flex justify-center">
@@ -136,11 +153,15 @@ export default function LoginPage({ status }: { status: string }) {
                   {/* Submit button */}
                   <div className="flex justify-center">
                     <button
-                      type="submit"
-                      className={clsx("w-[175px] border-2 border-cyan-600 hover:border-lime-600 text-green-950 dark:text-white font-bold py-2 mt-5 px-4 rounded-3xl mb-4", {
-                        "pointer-events-none opacity-50": authCard === "Sign In"
-                      })}
+                      className={clsx(
+                        "w-[175px] border-2 border-cyan-600 hover:border-lime-600 text-green-950 dark:text-white font-bold py-2 mt-5 px-4 rounded-3xl mb-4",
+                        {
+                          "pointer-events-none opacity-50":
+                            authCard === "Sign In",
+                        },
+                      )}
                       disabled={authCard === "Sign In"}
+                      type="submit"
                     >
                       Register
                     </button>
@@ -150,8 +171,8 @@ export default function LoginPage({ status }: { status: string }) {
                 <div className="flex justify-center">
                   <button
                     className="text-cyan-600 hover:text-lime-600 mt-4 text-center"
-                    onClick={toggleTwoAuth}
                     disabled={authCard === "Sign In"}
+                    onClick={toggleTwoAuth}
                   >
                     Already have an account? Sign In.
                   </button>
@@ -162,43 +183,44 @@ export default function LoginPage({ status }: { status: string }) {
             {/* Second card for login */}
             <div className="flex justify-center items-center">
               <motion.div
-                animate={{ opacity: authCard === "Sign Up" ? 0.5 : 1, scale: authCard === "Sign Up" ? 0.75 : 1 }}
-                transition={{ duration: 0.5 }}
+                animate={{
+                  opacity: authCard === "Sign Up" ? 0.5 : 1,
+                  scale: authCard === "Sign Up" ? 0.75 : 1,
+                }}
                 className={clsx(
                   "p-8 rounded-2xl shadow-lg shadow-cyan-600 hover:shadow-lime-500 w-full max-w-sm relative",
                   {
                     "right-28 blur-[2px] z-0": authCard === "Sign Up",
-                    "right-40 md:right-32 bg-green-100 dark:bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-[#0a0015] via-[#00150e] to-black z-30": authCard === "Sign In"
-                  }
+                    "right-40 md:right-32 bg-green-100 dark:bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-[#0a0015] via-[#00150e] to-black z-30":
+                      authCard === "Sign In",
+                  },
                 )}
+                transition={{ duration: 0.5 }}
               >
                 <form>
                   {/* Email input */}
                   <div className="mt-4">
                     <Input
-                      type="email"
-                      label="Email"
-                      placeholder="you@example.com"
-                      labelPlacement="outside"
-                      startContent={<FaEnvelope className="text-default-400" />}
                       disabled={authCard === "Sign Up"}
+                      label="Email"
+                      labelPlacement="outside"
+                      placeholder="you@example.com"
+                      startContent={<FaEnvelope className="text-default-400" />}
+                      type="email"
                     />
                   </div>
                   <br />
                   {/* Password input */}
                   <div className="mt-6">
                     <Input
-                      type={isVisible ? "text" : "password"}
-                      label="Password"
-                      placeholder="Enter your password"
-                      labelPlacement="outside"
-                      startContent={<FaLock className="text-default-400" />}
+                      className="md:w-[300px] w-[225px]"
+                      disabled={authCard === "Sign Up"}
                       endContent={
                         <button
+                          aria-label="toggle password visibility"
                           className="focus:outline-none"
                           type="button"
                           onClick={toggleVisibilityL}
-                          aria-label="toggle password visibility"
                           // disabled={authCard === "Sign Up"}
                         >
                           {isVisible ? (
@@ -208,15 +230,22 @@ export default function LoginPage({ status }: { status: string }) {
                           )}
                         </button>
                       }
-                      className="md:w-[300px] w-[225px]"
-                      disabled={authCard === "Sign Up"}
+                      label="Password"
+                      labelPlacement="outside"
+                      placeholder="Enter your password"
+                      startContent={<FaLock className="text-default-400" />}
+                      type={isVisible ? "text" : "password"}
                     />
                   </div>
                   <div className="flex justify-center">
                     <button
-                      className={clsx("text-cyan-600 text-sm mt-4 hover:text-lime-600 text-center", {
-                        "pointer-events-none opacity-50": authCard === "Sign Up"
-                      })}
+                      className={clsx(
+                        "text-cyan-600 text-sm mt-4 hover:text-lime-600 text-center",
+                        {
+                          "pointer-events-none opacity-50":
+                            authCard === "Sign Up",
+                        },
+                      )}
                       disabled={authCard === "Sign Up"}
                     >
                       Forgot your password?
@@ -225,11 +254,15 @@ export default function LoginPage({ status }: { status: string }) {
                   {/* Submit button */}
                   <div className="flex justify-center">
                     <button
-                      type="submit"
-                      className={clsx("w-[175px] border-2 border-cyan-600 hover:border-lime-600 text-green-950 dark:text-white font-bold py-2 mt-5 px-4 rounded-3xl mb-4", {
-                        "pointer-events-none opacity-50": authCard === "Sign Up"
-                      })}
+                      className={clsx(
+                        "w-[175px] border-2 border-cyan-600 hover:border-lime-600 text-green-950 dark:text-white font-bold py-2 mt-5 px-4 rounded-3xl mb-4",
+                        {
+                          "pointer-events-none opacity-50":
+                            authCard === "Sign Up",
+                        },
+                      )}
                       disabled={authCard === "Sign Up"}
+                      type="submit"
                     >
                       Login
                     </button>
@@ -239,15 +272,14 @@ export default function LoginPage({ status }: { status: string }) {
                 <div className="flex justify-center">
                   <button
                     className="text-cyan-600 hover:text-lime-600 mt-4 text-center"
-                    onClick={toggleTwoAuth}
                     disabled={authCard === "Sign Up"}
+                    onClick={toggleTwoAuth}
                   >
                     Don't have an account? Sign Up.
                   </button>
                 </div>
               </motion.div>
             </div>
-
           </div>
         </div>
       </div>
